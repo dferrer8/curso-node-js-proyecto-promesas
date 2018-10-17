cb410eb698fc1a17c17370401df36d2d299bb8ea
dferrer8 <david.dfg@gmail.com>
2018-10-17 16:53:28 +0200

    Funcionando reducido. Antes de async - await

--- index.2.js
+++ index.2.js
@@ -8,25 +8,22 @@ const exchangeRate = (from, to) => {
 
   // Make a request for a user with a given ID
   // restringido a base = EUR
-  return axios
+  axios
     .get(
       `http://data.fixer.io/api/latest?access_key=61fc16cfc7fe946197cb646e814ad031&base=EUR&symbols=${from},${to}`
     )
     .then(response => {
       const { rates } = response.data
-      // const fromDivisa = rates[from]
-      // const toDivisa = rates[to]
-
-      // return (toDivisa / fromDivisa)
-      return (rates[to] / rates[from]) // --- nos saltamos los pasos y enviamos directamente
+      const fromDivisa = rates[from]
+      const toDivisa = rates[to]
+      console.log(fromDivisa, toDivisa)
+      console.log(toDivisa / fromDivisa)
     })
-    // .catch(error => {
-    //   console.log(error)
-    // })
+    .catch(error => {
+      console.log(error)
+    })
 }
 
 // console.log(exchangeRate('EUR', 'USD'))
 // exchangeRate()
 exchangeRate('JPY', 'USD')
-  .then((cambioDivisa) => console.log(cambioDivisa))
-  .catch((err) => console.log(err))
